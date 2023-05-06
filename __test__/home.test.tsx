3ca99d1b0196659a393ae9993e9dcf06cfe0c5d4
argnivb <abelrecouso@gmail.com>
2023-05-05 20:22:51 -0400

    feat(config): add Prettier for code formatting.
    This commit adds Prettier as a dev dependency to the project, configures it to format code according to the project's style guide, and adds a script to the package.json file to make it easy to run Prettier. The configuration can be found in the `.prettierrc` file at the root of the project.

--- __test__/home.test.tsx
+++ __test__/home.test.tsx
@@ -1,14 +1,14 @@
-import { render, screen } from '@testing-library/react'
-import Home from '@/app/page'
+import { render, screen } from '@testing-library/react';
+import Home from '@/app/page';
 
 describe('Home', () => {
   it('renders a heading', () => {
-    render(<Home />)
+    render(<Home />);
 
     const heading = screen.getByRole('heading', {
       name: /Welcome to Next\.js 13/i,
-    })
+    });
 
-    expect(heading).toBeInTheDocument()
-  })
-})
+    expect(heading).toBeInTheDocument();
+  });
+});
