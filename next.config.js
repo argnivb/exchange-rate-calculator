fee5a7bc8ea57d20a4e9c388483c09369454baf0
argnivb <abelrecouso@gmail.com>
2023-06-08 14:15:54 -0400

    chore: Enable the styled-components SWC transform.

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
 
     return config;
   },
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
